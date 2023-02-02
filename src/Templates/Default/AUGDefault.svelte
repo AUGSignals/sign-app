e8859d1a98bb477316eab285a94aebe7d597a32f
jahnavi36 <jahnavi.munagala94@gmail.com>
2023-02-02 09:25:43 -0500

    change clipboard

--- src/Templates/Default/AUGDefault.svelte
+++ src/Templates/Default/AUGDefault.svelte
@@ -1,30 +1,30 @@
 <script>
- let message;
-
- import Employee from "../Employee.svelte";
- const outputString = "AUGDefault.svelte";
- 
-</script>
-
-<Employee/>
-  <img src= {"https://augsignals.com/wp-content/uploads/logo/aug-logo-200x110.jpg"} alt="auglogo" height="150px" width="250px"/>   
-  <hr />
-  <div class="footer">
-    <p>
-      NOTICE: Confidential message which may be privileged. Unauthorized use/disclosure prohibited. If received in error, please notify the sender immediately and delete all copies and attachments.
-    </p>
-    <p>
-      AVIS : Message confidentiel dont le contenu peut être privilégié. Utilisation/divulgation interdites sans permission. Si reçu par erreur, prière d'aviser immédiatement l'expéditeur et supprimer toutes les copies et les pièces jointes.
-    </p>
-  </div>
-  <button on:Click="{() => {
-    navigator.clipboard.writeText(outputString)
-    message.info("Copied!")
-  }}">
-    Copy to Clipboard
-  </button>
-     
+   import Employee from "../Employee.svelte";
+   import Clipboard from "svelte-clipboard";
    
+ 
+ </script>
+		
+	
+<Employee/>
+	<div class="footer">
+	  <p>
+		NOTICE: Confidential message which may be privileged. Unauthorized use/disclosure prohibited. If received in error, please notify the sender immediately and delete all copies and attachments.
+	  </p>
+	  <p>
+		AVIS : Message confidentiel dont le contenu peut être privilégié. Utilisation/divulgation interdites sans permission. Si reçu par erreur, prière d'aviser immédiatement l'expéditeur et supprimer toutes les copies et les pièces jointes.
+	  </p>
+	</div>
+  <Clipboard
+  text="hello worlld"
+  let:copy
+  on:copy={() => {
+    alert('Copied');
+  }}>
+  <button on:click={copy}>Copy to Clipboard</button>
+</Clipboard>
+      
+
 
 <style>
   .footer {
@@ -41,4 +41,5 @@
    background-color: transparent; 
    border-color:grey;
   }
+  
 </style>
